ae393519b2bfca04e63c5c2352ae9a6eaa1875d6
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-03 18:24:28 +0200

    fixed marker bug

--- public/ISS/index.js
+++ public/ISS/index.js
@@ -1,26 +1,4 @@
-let firstTime = true;
-
-//create async function to get data
-async function getData(url, map, marker){
-    const response = await fetch(url);
-    const data = await response.json();
-    // get desired features from json
-    const {latitude, longitude, velocity} = data;
-    //replace spans with data
-    document.getElementById('lat').textContent = latitude.toFixed(2);
-    document.getElementById('lon').textContent = longitude.toFixed(2);
-    document.getElementById('vel').textContent = velocity.toFixed(0);
-    //update view
-    if (firstTime) {
-        map.setView([latitude, longitude], 5);
-        firstTime = false;
-    }
-    
-    //update marker
-    marker.setLatLng([latitude, longitude]);
-}
-
-//creare map and tiles
+//create map and tiles
 //use tiles from openstreetmap
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const issMap = L.map('issMap', {minZoom: 2}).setView([0, 0], 2);
@@ -29,22 +7,42 @@ const tileOptions = {
 }
 const tiles = L.tileLayer(tileUrl, tileOptions)
 tiles.addTo(issMap)
-
 //set up icon
 const issIcon = L.icon({
     iconUrl: 'iss256.png',
     iconSize: [50, 32],
     iconAnchor: [25, 16],
 });
-
 //create marker
 let issMarker = L.marker([0, 0], {icon: issIcon}).addTo(issMap);
 
+// data url
 const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544"
+
+//create async function to get data
+let firstTime = true;
+async function getData(){
+    const response = await fetch(issApiUrl);
+    const data = await response.json();
+    // get desired features from json
+    const {latitude, longitude, velocity} = data;
+    //update marker
+    issMarker.setLatLng([latitude, longitude]);
+    //replace spans with data
+    document.getElementById('lat').textContent = latitude.toFixed(2);
+    document.getElementById('lon').textContent = longitude.toFixed(2);
+    document.getElementById('vel').textContent = velocity.toFixed(0);
+    //update view
+    if (firstTime) {
+        issMap.setView([latitude, longitude], 5);
+        firstTime = false;
+    }
+}
+
 //first run
-getData(issApiUrl, issMap, issMarker)
+getData();
 // repeat function every 5 seconds
 setInterval(() => { 
-    getData(issApiUrl);
+    getData();
     console.log("Updated")
 }, 1000);
