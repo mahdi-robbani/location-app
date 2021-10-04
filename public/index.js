1844b96c90c05d94ceab2cd4deeb44c131c20d62
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-04 14:13:13 +0200

    Added map to index

--- public/index.js
+++ public/index.js
@@ -1,3 +1,21 @@
+//create map and tiles
+//use tiles from openstreetmap
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const issMap = L.map('issMap', {minZoom: 2}).setView([0, 0], 2);
+const tileOptions = {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}
+const tiles = L.tileLayer(tileUrl, tileOptions)
+tiles.addTo(issMap)
+// //set up icon
+// const issIcon = L.icon({
+//     iconUrl: 'iss256.png',
+//     iconSize: [50, 32],
+//     iconAnchor: [25, 16],
+// });
+//create marker
+let issMarker = L.marker([0, 0]).addTo(issMap);
+
 function geolocate(){
   if('geolocation' in navigator) {
     console.log('Geolocation is available')
