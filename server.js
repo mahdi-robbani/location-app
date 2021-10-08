71e5667654253435c9ab81c287dc14b94bc2cf93
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-08 20:17:13 +0200

    Added mongoDB datbase

--- server.js
+++ server.js
@@ -18,8 +18,60 @@ app.post('/api', (request, response) => {
         status: "success",
         data: request.body,
     })
-    updateLocalFile(request)
+    //updateLocalFile(request)
+    accessDb(request)
 });
+
+async function accessDb(request){
+    //db url
+    const url = 'mongodb://127.0.0.1:27017'
+    //new instance of mongo client
+    const client = new MongoClient(url);
+    try{
+        //connect to client
+        await client.connect();
+
+        //list databases
+        //await listDatabases(client);
+
+        //update database
+        newLocation = {
+            latitude: request.body.latitude,
+            longitude: request.body.longitude,
+            date: Date.now(),
+        }
+        await createLocation(client, newLocation);
+
+        //list all entries
+        await showAllLocations(client)
+
+    } catch (e) {
+        console.error(e)
+    } finally {
+        //close connection after performing all operations
+        await client.close()
+    }
+}
+
+async function createLocation(client, newLocation){
+    const result = await client.db("selfie-data-app").collection("locations").insertOne(newLocation);
+    console.log(`New location ID: ${result.insertedId}`);
+}
+
+async function listDatabases(client) {
+    //function to list all databases
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => {
+        console.log(` - ${db.name}`)
+    });
+};
+
+async function showAllLocations(client){
+    const cursor = await client.db("selfie-data-app").collection("locations").find();
+    const result = await cursor.toArray()
+    result.forEach(document => console.log(document))
+}
 
 function updateLocalFile(request){
     fs.readFile("location.txt", 'utf-8', (err, data) => {
