12d80160512abd98d625bc79f9ebd8dbc7cbf11e
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-10 17:31:39 +0200

    displayed data on page

--- public/all.js
+++ public/all.js
@@ -1,7 +1,30 @@
 async function getData(){
     const request = await fetch('api/');
     const data = await request.json();
-    console.log(data);
+
+    // data.forEach((elem, ind) => {
+    //     console.log(elem, elem)
+    // })
+
+    for (item of data){
+        //create elements and sub elements
+        const root = document.createElement('div')
+        const loc = document.createElement('div')
+        const city = document.createElement('div')
+        const date = document.createElement('div')
+
+        //Add data to elements
+        loc.textContent = `Latitude: ${item.latitude}, Longitude: ${item.longitude}`
+        cityString = item.city ? item.city : 'None'
+        city.textContent = `City: ${cityString}`
+        const dateString = new Date(item.date).toLocaleString();
+        date.textContent = `Date: ${dateString}`
+
+        //Append sub elements to main element
+        root.append(loc, city, date)
+        //Add to page
+        document.body.append(root)
+    }
 }
 
 getData();
