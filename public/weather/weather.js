560c7c2c275ab5d8f7dc71b65e517985e7510517
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-18 16:37:12 +0200

    dynamically generated air quality table

--- public/weather/weather.js
+++ public/weather/weather.js
@@ -15,13 +15,61 @@ function geolocate(){
     console.log('Geolocation is available')
     navigator.geolocation.getCurrentPosition( async (position) => {
       const {latitude, longitude} = position.coords
-      //console.log(position.coords.latitude, position.coords.longitude);
       //update marker
       geoMarker.setLatLng([latitude, longitude]);
       //update view
       geoMap.setView([latitude, longitude], 10);
-      //send data to server
+
+      //send get request to server to retreive weather + aq information
+      const response = await fetch(`/weather/${latitude}/${longitude}`);
+      const json = await response.json();
+
+      //parse weather data
+      const weather = json.weather.weather['0'].main
+      const temp = (json.weather.main.temp - 273.15)
+      const feels = (json.weather.main.feels_like - 273.15)
+      const aqResults = json.aq.results
+      //parse air quality data
+      let aqParam, aqQuality, aqValue, aqUnit;
+      if (aqResults.length > 0){
+        const measurements = aqResults[0].measurements
+        measurements.forEach((elem, ind) => {
+          // get values
+          const parameter = elem.parameter
+          const value = elem.value
+          const unit = elem.unit
+          
+          //insert into table
+          const tbody = document.getElementById("locationtablebody")
+          const tr = tbody.insertRow();
+          const tdName = tr.insertCell();
+          tdName.appendChild(document.createTextNode(`${parameter}:`));
+          const tdInfo = tr.insertCell();
+          tdInfo.appendChild(document.createTextNode(`${value}${unit}`));
+        })
+      }
+
+      // console.log(aqResults.length)
+      // console.log(aqResults)
+      //add info to webpage
+      document.getElementById('lat').textContent = `${latitude}`;
+      document.getElementById('lon').textContent = `${longitude}`;
+      document.getElementById('temp').textContent = `${temp.toFixed(2)}°C`;
+      document.getElementById('feels').textContent = `${feels.toFixed(2)}°C`;
+      document.getElementById('weather').textContent = weather;
+      // try{
+      //   //get aq data
+      //   const aq = json.aq.results[0].measurements[0]
+      //   document.getElementById('aq_param').textContent = ` (${aq.parameter}):`;
+      //   document.getElementById('aq_quality').textContent = `${aq.value}${aq.unit}`;
+      // } catch (error) {
+      //   console.error(error);
+      //   document.getElementById('aq_quality').textContent = "No Data";
+      // }
+
+      //send data to server (store in db)
       const data = {latitude, longitude};
+      console.log(data)
       const options = {
         method: 'POST',
         headers: {
@@ -29,29 +77,6 @@ function geolocate(){
         },
         body: JSON.stringify(data),
       };
-      //send get request to server to retreive weather information
-      const response = await fetch(`/weather/${latitude}/${longitude}`);
-      const json = await response.json();
-      console.log(json)
-      //get weather data
-      const weather = json.weather.weather['0'].main
-      const temp = (json.weather.main.temp - 273.15).toFixed(2)
-      const feels = (json.weather.main.feels_like - 273.15).toFixed(2)
-      //add info to webpage
-      document.getElementById('lat').textContent = `${latitude}`;
-      document.getElementById('lon').textContent = `${longitude}`;
-      document.getElementById('temp').textContent = `${temp}°C`;
-      document.getElementById('feels').textContent = `${feels}°C`;
-      document.getElementById('weather').textContent = weather;
-      try{
-        //get aq data
-        const aq = json.aq.results[0].measurements[0]
-        document.getElementById('aq_param').textContent = ` (${aq.parameter}):`;
-        document.getElementById('aq_quality').textContent = `${aq.value}${aq.unit}`;
-      } catch (error) {
-        console.error(error);
-        document.getElementById('aq_quality').textContent = "No Data";
-      }
 
       //console.log(aq)
   });
