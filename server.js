bed35d4525e4298f0122ad8e13a2150f002ba0e0
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-19 18:06:31 +0200

    changed port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 //get port from environment variable or user localhost:3000
 const port = process.env.PORT || 3000
 //start listening
-app.listen(3000, () => console.log(`Listening at port: ${port}`))
+app.listen(port, () => console.log(`Listening at port: ${port}`))
 //use the express library to host static files
 app.use(express.static('public'));
 //enable ability to receive json data
